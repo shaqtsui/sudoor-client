9b48df470df8b5da99bb76e8a8e7377a9a0508c1
Shark <xfcjscn@gmail.com>
2014-01-04 12:20:34 +0800

    add init files

---
+++ app/scripts/models/requirejs.config.js
@@ -0,0 +1,93 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-11-29
+ * Time: 下午12:05
+ * To change this template use File | Settings | File Templates.
+ */
+require.config({
+
+	//baseUrl value currently relative to outer html page, suppose to change it to base on config script location
+	baseUrl: '../../bower_components',
+
+	//only for setting up root paths for module IDs, the module not changed
+	paths: {
+		//For scripts & styles
+		scripts: '../scripts',
+		vendor: '../scripts/vendor',
+		models: '../scripts/models',
+		styles: '../styles',
+
+		//For named module
+		jquery: 'jquery/jquery',
+		datajs: '../scripts/vendor/datajs/datajs-1.1.1',
+		OData: '../scripts/vendor/datajs/datajs-1.1.1'
+	},
+	map: {
+		//Map one module to another, so that relative module ID will resolve base on mapped module
+		'*': {
+			//Plugin
+			css: 'require-css/css',
+			domReady: 'requirejs-domready/domReady',
+			text: 'requirejs-text/text',
+			//Legacy, pls use shim config instead
+			depend: 'requirejs-plugins/src/depend',
+			json: 'requirejs-plugins/src/json'
+		}
+	},
+	shim: {
+		'swipe/swipe': ['jquery', 'css!styles/vendor/swipe/swipe'],
+		'jquery-ui/ui/jquery-ui': ['jquery', 'css!jquery-ui/themes/base/jquery-ui'],
+		'purl/purl': ['jquery'],
+		'bootstrap/dist/js/bootstrap': ['jquery', 'css!bootstrap/dist/css/bootstrap', 'css!bootstrap/dist/css/bootstrap-theme'],
+		'simplecart-js/simpleCart': ['jquery'],
+		'foundation/js/foundation': ['jquery', 'modernizr/modernizr', 'css!foundation/css/foundation'],
+		'foundation/js/foundation.min': ['jquery', 'modernizr/modernizr', 'css!foundation/css/foundation'],
+
+		'parsleyjs/parsley': ['jquery'],
+		'parsleyjs/parsley.extend': ['parsleyjs/parsley'],
+		'parsleyjs/l10n/parsley.es': ['parsleyjs/parsley'],
+		'parsleyjs/i18n/messages.zh_cn': ['parsleyjs/parsley'],
+
+		'plupload/js/plupload.dev': ['plupload/js/moxie'],
+		'plupload/js/jquery.ui.plupload/jquery.ui.plupload': ['jquery-ui/ui/jquery-ui', 'plupload/js/plupload.dev', 'css!plupload/js/jquery.ui.plupload/css/jquery.ui.plupload'],
+		'plupload/js/i18n/zh_CN': ['plupload/js/jquery.ui.plupload/jquery.ui.plupload'],
+
+		//JQM
+		"jquery.hashchange": {
+			deps: [ "jquery" ]
+		},
+		"jquery.ui.widget": {
+			deps: [ "jquery" ],
+			exports: "$.widget"
+		},
+		"widgets/jquery.ui.tabs": {
+			deps: [ "jquery.ui.widget" ]
+		},
+		"widgets/jquery.ui.core": {
+			deps: [ "jquery" ],
+			exports: [ "$.ui" ]
+		},
+
+		'vendor/jaydata/jaydata': ['OData'],
+		'underscore.string/lib/underscore.string': ['underscore/underscore'],
+
+		//Angular
+		'angular/angular': ['jquery'],
+		'angular-route/angular-route': ['angular/angular'],
+		'angular-resource/angular-resource': ['angular/angular'],
+		'angular-animate/angular-animate': ['angular/angular'],
+		'angular-loading-bar/src/loading-bar': ['angular/angular', 'css!angular-loading-bar/src/loading-bar'],
+		'angular-http-auth/src/http-auth-interceptor': ['angular/angular'],
+		'nginfinitescroll/build/ng-infinite-scroll': ['angular/angular'],
+		'angular-translate/angular-translate': ['angular/angular'],
+		'angular-translate-loader-static-files/angular-translate-loader-static-files': ['angular-translate/angular-translate'],
+		'angular-xeditable/dist/js/xeditable': ['angular/angular', 'css!angular-xeditable/dist/css/xeditable'],
+		'ngAnimate-animate.css/animate': ['angular-animate/angular-animate'],
+
+		//works with angular 1.1.5 for $AnimateProvider issue
+		'angular-jqm/components/angular/angular-mobile': ['angular-animate/angular-animate'],
+		'angular-jqm/dist/angular-jqm': ['angular/angular', 'angular-jqm/components/angular/angular-mobile', 'css!jquery-mobile/css/themes/default/jquery.mobile']
+
+	}
+});
---
+++ app/scripts/models/server.js
@@ -0,0 +1,129 @@
+define(['q/q', 'jquery', 'purl/purl', 'OData', 'vendor/jaydata/jaydata'], function(Q, $, purl, OData) {
+	//JayData need global OData
+	window.OData = OData;
+
+	var server = {};
+	var clientURL = $.url();
+
+	/*
+	 server.config  maintain the config data
+	 */
+	server.config = {
+		serverURL: clientURL.attr('protocol') + '://' + clientURL.attr('host').replace("www.", "server.") + ':80/gng-server-1.0',
+		odataURI: '/data/odata.svc',
+		userURI: '/data/odata.svc/User',
+		loginURI: '/j_spring_security_check',
+		logoutURI: '/j_spring_security_logout',
+		authenticationURI: '/data/rest/SpringSecurity/Authentication',
+		fileUploadURL : '/data/rest/tools/fileupload/File'
+	};
+
+	server._db = null;
+
+	/*
+	 server.getDb() return DB promise with cache
+	 */
+	server.getDb = function() {
+		if (!server._db) {
+			//Promise Fulfillment
+			var servicePromise = $data.initService(server.config.serverURL + server.config.odataURI);
+			var dbCacheDeferred = Q.defer();
+			servicePromise.then(function(remoteDBContext, contextFactory, contexType) {
+				var localDBContext = contextFactory({
+					name: 'local',
+					databaseName: 'GPlatformDB'
+				});
+				dbCacheDeferred.resolve(localDBContext);
+			});
+			server._db = Q(servicePromise);
+			server._db.cache = dbCacheDeferred.promise;
+		}
+		return  server._db;
+	};
+
+	server._session = null;
+
+	/*
+	 server.getSession() return Session promise
+	 */
+	server.getSession = function() {
+		if (!server._session) {
+			var authMsg = $.ajax({
+						url: server.config.serverURL + server.config.authenticationURI,
+						type: 'GET',
+						xhrFields: {
+							withCredentials: true
+						}
+					}
+			);
+			server._session = Q(authMsg).then(function(data) {
+                var sessionData = {
+                    user: data
+                };
+                return sessionData;
+            });
+		}
+		return server._session;
+	};
+
+	/*
+	 server.destroySession() destroy session, so that can get fresh one via server.getSession()
+	 */
+	server.destroySession = function() {
+		server._session = null;
+	};
+
+	/*
+	 server.login() fire login msg & update session
+	 */
+	server.login = function(loginData) {
+		var loginMsg = $.ajax({
+					url: server.config.serverURL + server.config.loginURI,
+					type: 'POST',
+					data: loginData,
+					xhrFields: {
+						withCredentials: true
+					}
+				}
+		);
+		var promise = Q(loginMsg).then(function() {
+			server.destroySession();
+			return server.getSession();
+		});
+		return promise;
+	};
+
+	/*
+	 server.logout() fire logout msg & update session
+	 */
+	server.logout = function() {
+		var logoutMsg = $.ajax({
+					url: server.config.serverURL + server.config.logoutURI,
+					type: 'GET',
+					xhrFields: {
+						withCredentials: true
+					}
+				}
+		);
+		var promise = Q(logoutMsg).then(
+				function() {
+					server.destroySession();
+					return server.getSession();
+				}
+		);
+		return promise;
+	};
+
+	/*
+	 server.isLogin() check whether current session login
+	 */
+	server.isLogin = function() {
+		var data = server.getSession().inspect().value;
+		if (data) {
+			return (data.user.name != 'anonymousUser');
+		}
+		return false;
+	};
+
+	return server;
+});
