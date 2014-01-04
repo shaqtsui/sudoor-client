5cbf4b051f413aab432b1071c55b02952c32b9bf
Shark <xfcjscn@gmail.com>
2014-01-04 12:35:51 +0800

    change infra.css reference

--- app/scripts/models/infra.js
+++ app/scripts/models/infra.js
@@ -1,4 +1,4 @@
-define(['models/validator', 'parsleyjs/parsley.extend', 'parsleyjs/l10n/parsley.es', 'parsleyjs/i18n/messages.zh_cn', 'jquery-mobile/js/widgets/panel', 'css!jquery-mobile/css/themes/default/jquery.mobile', 'css!normalize-css/normalize', 'css!font-awesome/css/font-awesome', 'css!animate.css/animate', 'css!styles/gplatform-lib', 'swipe/swipe', 'bootstrap/dist/js/bootstrap', 'underscore.string/lib/underscore.string', 'modernizr/modernizr', 'simplecart-js/simpleCart'], function () {
+define(['models/validator', 'parsleyjs/parsley.extend', 'parsleyjs/l10n/parsley.es', 'parsleyjs/i18n/messages.zh_cn', 'jquery-mobile/js/widgets/panel', 'css!jquery-mobile/css/themes/default/jquery.mobile', 'css!normalize-css/normalize', 'css!font-awesome/css/font-awesome', 'css!animate.css/animate', 'css!styles/infra', 'swipe/swipe', 'bootstrap/dist/js/bootstrap', 'underscore.string/lib/underscore.string', 'modernizr/modernizr', 'simplecart-js/simpleCart'], function () {
 	var info = {components: ['bootstrap', 'foundation', 'underscore.string', 'modernizr', 'simpleCart']};
 	return info;
 });
