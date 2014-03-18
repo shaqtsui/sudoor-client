c3cdab780c0d311dd753af044c41e9c024af820d
Shark <xfcjscn@gmail.com>
2014-03-18 22:29:13 +0800

    add ./models/server to cache

--- app/scripts/requirejs.config.cache.js
+++ app/scripts/requirejs.config.cache.js
@@ -1,6 +1,4 @@
-define(['./models/infra', './models/angular', './models/postload', './models/validator'], function () {
+//Must use enhanced jaydata(resolve provider path issue)
+define(['./models/infra', './models/angular', './models/server', './models/postload', './models/validator'], function () {
 	return {};
 });
-
-//Can not include ./vendor/jaydata/jaydata, as there will be path issue: /angular_comp/GPlatform/jaydataproviders/oDataProvider.js
-//Can not include './models/server' as it rely on jaydata
