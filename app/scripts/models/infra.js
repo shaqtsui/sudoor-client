f9c450bd509c65dc752754a79f398fe902f35105
Shark <xfcjscn@gmail.com>
2014-04-11 20:12:36 +0800

    add blockui

--- app/scripts/models/infra.js
+++ app/scripts/models/infra.js
@@ -1,5 +1,8 @@
-define(['swiper/dist/idangerous.swiper', './validator',  'css!normalize-css/normalize', 'css!font-awesome/css/font-awesome', 'css!animate.css/animate', 'css!../../styles/infra', 'swipe/swipe', 'bootstrap/dist/js/bootstrap', 'underscore/underscore', 'underscore.string', 'modernizr/modernizr', 'simplecart-js/simpleCart'], function () {
-    var info = {components: ['bootstrap', 'foundation', 'underscore.string', 'modernizr', 'simpleCart']};
-    return info;
+define([ 'blockui/jquery.blockUI', 'swiper/dist/idangerous.swiper', './validator', 'css!normalize-css/normalize',
+        'css!font-awesome/css/font-awesome', 'css!animate.css/animate', 'css!../../styles/infra', 'swipe/swipe', 'bootstrap/dist/js/bootstrap',
+        'underscore/underscore', 'underscore.string', 'modernizr/modernizr', 'simplecart-js/simpleCart' ], function() {
+	var info = {
+		components : [ 'bootstrap', 'foundation', 'underscore.string', 'modernizr', 'simpleCart' ]
+	};
+	return info;
 });
-
