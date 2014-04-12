41a1d07f163487fc9149e8f62d34b6457b8fac82
Shark <xfcjscn@gmail.com>
2014-04-12 16:59:32 +0800

    disable log by default

--- app/scripts/models/logging.js
+++ app/scripts/models/logging.js
@@ -1,22 +1,21 @@
 define([ 'infra-client/app/scripts/vendor/log4javascript/log4javascript' ], function() {
+	//Can enable logging via script injection
+	log4javascript.setEnabled(false);
+	
 	// Create the logger
 	var log = log4javascript.getLogger();
 
-//	// Create a PopUpAppender with default options
-//	var popUpAppender = new log4javascript.PopUpAppender();
-//
-//	// Change the desired configuration options
-//	popUpAppender.setFocusPopUp(true);
-//	popUpAppender.setNewestMessageAtTop(true);
-//
-//	// Add the appender to the logger
-//	log.addAppender(popUpAppender);
+	// Create a PopUpAppender with default options
+	var popUpAppender = new log4javascript.PopUpAppender();
+	// Change the desired configuration options
+	popUpAppender.setFocusPopUp(true);
+	popUpAppender.setNewestMessageAtTop(true);
+	// Add the appender to the logger
+	log.addAppender(popUpAppender);
 
 	var browserConsoleAppender = new log4javascript.BrowserConsoleAppender();
 	log.addAppender(browserConsoleAppender);
-
-	var inPageAppender = new log4javascript.InPageAppender();
-	log.addAppender(inPageAppender);
-
+	
+	
 	return log;
 });
