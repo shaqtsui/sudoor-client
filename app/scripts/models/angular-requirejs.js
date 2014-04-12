0bfc89ce1cdced3359db3e92e7e052b3d99d2650
Shark <xfcjscn@gmail.com>
2014-04-12 22:51:17 +0800

    support require 3 parameters

--- app/scripts/models/angular-requirejs.js
+++ app/scripts/models/angular-requirejs.js
@@ -1,18 +1,35 @@
 define([ 'extendFunction', './logging', 'angular/angular' ], function(extendFunction, log) {
 	angular.module('angular-requirejs', []).run(function($rootScope) {
-		// Extend require to call enhanced callback
-		extendFunction('require', function(args, originalFunction, dontCallOriginal) {
-			// Extend callback to call $rootScope.$apply() after it be executed
-			var dep = args[0];
-			if (args.length == 2) {
-				var reqCB = args[1];
-				var eReqCB = extendFunction(reqCB, function(a, b, c) {
-					b.apply(null, a)
+
+		// Extend Function to call $rootScope.$apply() after it be executed
+		// Since each time the callback is a new function, so can not do cache
+		var extendFuncInvokeApply = (function() {
+			return function(func) {
+					var eFunc = extendFunction(func, function(a, b, c) {
+						var res = b.apply(this, a);
 					$rootScope.$apply();
-				});
-				log.debug('Callback for: [' + dep + '] extended to call $rootScope.$apply()');
+						return res;
+					});
+					log.debug('Extend Func: [' + func + '] to Invoke Apply');
+				return eFunc;
+			};
+		})();
+
+		// Extend require to call enhanced callback
+		require = extendFunction(require, function(args, originalFunction, dontCallOriginal) {
+			var dep = args[0];
+			var sCB = args[1];
+			var fCB = args[2];
+
+			if (sCB) {
+				var eSCB = extendFuncInvokeApply(sCB);
 			}
-			originalFunction.call(window, dep, eReqCB);
+			if (fCB) {
+				var eFCB = extendFuncInvokeApply(fCB);
+			}
+
+			var result = originalFunction.call(this, dep, eSCB, eFCB);
+			return result;
 		});
 
 		log.debug('Extended require to call enhanced callback');
