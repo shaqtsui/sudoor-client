b51d9c94ae9e488449eb5a66615e827d30659bb5
Shark <xfcjscn@gmail.com>
2014-04-12 13:14:11 +0800

    extend require

--- app/scripts/models/angular-requirejs.js
+++ app/scripts/models/angular-requirejs.js
@@ -1,10 +1,20 @@
 define([ 'extendFunction', 'angular/angular' ], function(extendFunction) {
-	angular.module('angular-requirejs', [])
-
-	.run(function($rootScope) {
-		extendFunction(require.onResourceLoad, function() {
-			console.log('Extend onResourceLoad');
-			$rootScope.$apply();
+	angular.module('angular-requirejs', []).run(function($rootScope) {
+		// Extend require to call enhanced callback
+		extendFunction('require', function(args, originalFunction, dontCallOriginal) {
+			// Extend callback to call $rootScope.$apply() after it be executed
+			var dep = args[0];
+			if (args.length == 2) {
+				var reqCB = args[1];
+				var eReqCB = extendFunction(reqCB, function(a, b, c) {
+					b.apply(null, a)
+					$rootScope.$apply();
+				});
+				console && console.log('Callback for: [' + dep + '] extended to call $rootScope.$apply()');
+			}
+			originalFunction.call(window, dep, eReqCB);
 		});
+
+		console && console.log('Extended require to call enhanced callback');
 	});
 });
