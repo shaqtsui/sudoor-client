db78bfb4431848a0748e05c65c83c57d3ffb5b99
Shark <xfcjscn@gmail.com>
2014-04-29 19:39:02 +0800

    add login() test

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,13 +3,24 @@
 define([ 'sudoor-client/app/scripts/models/server' ], function(server) {
 	describe('server', function() {
 		describe('#login()', function() {
-			it('should run here few assertions', function() {
-
+			it('should return SUCCESS when correct credentials used', function(done) {
+				var loginData = {
+				    j_username : "test",
+				    j_password : "test",
+				    _spring_security_remember_me : "false"
+				};
+				return expect(server.login(loginData)).to.eventually.equal('SUCCESS');
+				// server.login(loginData).then(function(result) {
+				// expect(result).to.equal("SUCCESS");
+				// done();
+				// }, function(err) {
+				// done(err);
+				// });
 			});
 		});
 		describe('#isLogin()', function() {
 			it('should return false when not login', function() {
-				server.isLogin().should.equal(false);
+				expect(server.isLogin()).to.equal(false);
 			});
 		});
 	});
