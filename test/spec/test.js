03d71b7100bb8afceb5f38aca5d05384b58a1a55
Shark <xfcjscn@gmail.com>
2014-04-29 14:52:29 +0800

    test server.isLogin()

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,18 @@
 /*global describe, it */
 'use strict';
-(function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+require([ 'sudoor-client/app/scripts/models/server' ], function(server) {
+	describe('server', function() {
+		describe('#login()', function() {
+			it('should run here few assertions', function() {
 
-            });
-        });
-    });
-})();
+			});
+		});
+		describe('#isLogin()', function() {
+			it('should return false when not login', function() {
+				server.isLogin().should.equal(false);
+			});
+		});
+	});
+
+	mocha.run();
+});
